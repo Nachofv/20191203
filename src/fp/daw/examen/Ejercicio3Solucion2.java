e86c0ecc97d79a824c836726d652823d25e579fa
Julio Pedro López Bayón <jplbayon@gmail.com>
2019-12-05 19:25:40 +0100

    Ejercicio 3 solucionado

--- src/fp/daw/examen/Ejercicio3Solucion2.java
+++ src/fp/daw/examen/Ejercicio3Solucion2.java
@@ -2,8 +2,8 @@ package fp.daw.examen;
 
 import java.util.Scanner;
 
-public class Ejercicio3 {
-
+public class Ejercicio3Solucion2 {
+	
 	/* 3 puntos
 	 * 
 	 * Decimos que un número entero positivo es guay si puede obtenerse como suma de dos o más números
@@ -11,7 +11,10 @@ public class Ejercicio3 {
 	 * 
 	 * Escribir en el método main un programa que lea un número entero positivo e indique si éste es guay.
 	 * 
+	 * Esta solución amplia lo que se pide originalmente mostrando qué números son guays desde 0 hasta el
+	 * número que introduzca el usuario.
 	 * 
+	 * También se optimiza algo el código en detrimento de la legibilidad, como suele ser habitual.
 	 */
 	
 	public static void main(String[] args) {
@@ -23,35 +26,35 @@ public class Ejercicio3 {
 			if (n < 0)
 				System.out.println("Número incorrecto, inténtalo de nuevo.");
 		} while (n < 0);
-		
+		for (int i=0; i<=n; i++)
+			esGuay(i);
+		in.close();
+	}
+	
+	static void esGuay(int n) {
 		int i = 1, j = 0, n2 = n / 2;
 		boolean esGuay = false;
 		while (i <= n2 && !esGuay) {
-			int suma = 0;
-			j = i;
-			while (j < n && !esGuay) {
-				suma += j;
+			int suma = j = i;
+			while (suma < n && !esGuay) {
+				suma += ++j;
 				if (suma == n)
 					esGuay = true;
-				else
-					j++;
 			}
-			if (!esGuay)
+			if (suma == n)
+				esGuay = true;
+			else
 				i++;
 		}
 		System.out.print(String.valueOf(n));
 		if (esGuay) {
 			System.out.print(" = ");
-			for (int k=i; k<=j; k++) {
-				System.out.print(String.valueOf(k));
-				if (k < j)
-					System.out.print(" + ");
-			}
+			for (int k=i; k<=j; k++)
+				System.out.print(k + (k < j ? " + " : ""));
 		}
 		else
 			System.out.print(" no");
 		System.out.println(" es guay");
-		in.close();
 	}
 
 }
